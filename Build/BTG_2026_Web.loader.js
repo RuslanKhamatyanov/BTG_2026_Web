36684262cd24a5e426dd819afb1192b6c2b4d02d
Руслан Хаматьянов <ruslan.khamatyanov@fluytstudio.net>
2026-07-26 21:40:23 +0300

    Fix.

--- Build/BTG_2026_Web.loader.js
+++ Build/BTG_2026_Web.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 68854034,
+    wasmFileSize: 68854050,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
