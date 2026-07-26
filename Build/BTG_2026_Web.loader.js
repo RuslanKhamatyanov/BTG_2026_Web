8c5e376740df1e1a66dc639769291c85f4bd941b
Руслан Хаматьянов <ruslan.khamatyanov@fluytstudio.net>
2026-07-26 12:09:31 +0300

    Анимация бега.

--- Build/BTG_2026_Web.loader.js
+++ Build/BTG_2026_Web.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 68764659,
+    wasmFileSize: 68771398,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
